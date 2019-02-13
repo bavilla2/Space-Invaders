d5325ad243b1318cdb1b90b959abdb3df92d2b26
Billy <billyanthonyvilla@gmail.com>
2019-02-13 02:31:57 -0800

    Fixed merge conflicts with SampleScene and added Pause, Main Menu, Game, Quit, Play Game

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pausePanel;
+    bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    //Quit the application when user clicks button
+    public void OnClickQuit()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
